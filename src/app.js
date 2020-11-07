b1f3d49da619ab77614000b3cd0ecfeebc6f63a6
Wallace Saturnino <wallacesat@live.co.uk>
2020-11-06 23:26:52 -0300

    Desafio Concluído

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,58 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  repositories.push({ id: uuid(), title, url, techs, likes: 0 });
+  return response.json(repositories[repositories.length-1]);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id} = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0 ) {
+    return response.status(400).json({ error: "Repositório não encontrado"});
+  }
+
+  repositories[repoIndex] = { ...repositories[repoIndex], ...{ title, url, techs }}
+
+  return response.json(repositories[repoIndex]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id} = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0 ) {
+    return response.status(400).json({ error: "Repositório não encontrado"});
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id} = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0 ) {
+    return response.status(400).json({ error: "Repositório não encontrado"});
+  }
+
+  repositories[repoIndex] = { ...repositories[repoIndex], ...{ likes: repositories[repoIndex].likes+1 }};
+
+  return response.json(repositories[repoIndex]);
 });
 
 module.exports = app;
